7522c634a9130dc978422d37bad4ba1bbcf1e091
Your Name <you@example.com>
2025-06-18 23:09:26 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// function handleSubmit() {
+//   alert("Thank you for your message!");
+//   return false; // Prevent actual form submission
+// }
+
+// Contact form submission (fake alert for now)
+function handleSubmit() {
+  alert("Thank you for your message!");
+  return false; // Prevents actual form submission
+}
+
+// Dark Mode Toggle
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
